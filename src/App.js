fb1a724bb3058cc43029a9f17730a11a35d66106
delia-pixel <tpatriciareinedia@gmail.com>
2022-06-30 11:06:28 +0100

    about & resume

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import "./App.css";
+//import "./App.css";
 import "./assets/css/style.css";
-
 import Header from "./components/Header";
+import About from "./components/About";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <About/>
+        <Resume/>
     </div>
   );
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,30 +3,30 @@ import React from "react";
 const Header = () => {
   return (
     <header id="header">
-			<div class="container">
+			<div className="container">
 				<h1><a href="index.html">Emily Jones</a></h1>
-				<a href="index.html" class="mr-auto">
-					<img src="../assets/img/logo.png" alt="" class="img-fluid" />
+				<a href="index.html" className="mr-auto">
+					<img src="../assets/img/logo.png" alt="" className="img-fluid" />
 				</a>
 				<h2>I'm a passionate <span>graphic designer</span> from New York</h2>
 
-				<nav id="navbar" class="navbar">
+				<nav id="navbar" className="navbar">
 					<ul>
-						<li><a class="nav-link active" href="#header">Home</a></li>
-						<li><a class="nav-link" href="#about">About</a></li>
-						<li><a class="nav-link" href="#resume">Resume</a></li>
-						<li><a class="nav-link" href="#services">Services</a></li>
-						<li><a class="nav-link" href="#portfolio">Portfolio</a></li>
-						<li><a class="nav-link" href="#contact">Contact</a></li>
+						<li><a className="nav-link active" href="#header">Home</a></li>
+						<li><a className="nav-link" href="#about">About</a></li>
+						<li><a className="nav-link" href="#resume">Resume</a></li>
+						<li><a className="nav-link" href="#services">Services</a></li>
+						<li><a className="nav-link" href="#portfolio">Portfolio</a></li>
+						<li><a className="nav-link" href="#contact">Contact</a></li>
 					</ul>
-					<i class="bi bi-list mobile-nav-toggle"></i>
+					<i className="bi bi-list mobile-nav-toggle"></i>
 				</nav>
 
-				<div class="social-links">
-					<a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-					<a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-					<a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-					<a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+				<div className="social-links">
+					<a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+					<a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+					<a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+					<a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
 				</div>
 
 			</div>
