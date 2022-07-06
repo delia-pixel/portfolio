4a7c069310af2ded56d19885e21ee1998f9d78c1
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-07-06 17:33:43 +0200

    rename Header component to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,11 +13,11 @@ const Home = () => {
 				<nav id="navbar" className="navbar">
 					<ul>
 						<li><a className="nav-link active" href="#header">Home</a></li>
-						<li><a className="nav-link" href="#about">About</a></li>
-						<li><a className="nav-link" href="#resume">Resume</a></li>
-						<li><a className="nav-link" href="#services">Services</a></li>
-						<li><a className="nav-link" href="#portfolio">Portfolio</a></li>
-						<li><a className="nav-link" href="#contact">Contact</a></li>
+						<li><a className="nav-link" href="/about">About</a></li>
+						<li><a className="nav-link" href="/resume">Resume</a></li>
+						<li><a className="nav-link" href="/services">Services</a></li>
+						<li><a className="nav-link" href="/portfolio">Portfolio</a></li>
+						<li><a className="nav-link" href="/contact">Contact</a></li>
 					</ul>
 					<i className="bi bi-list mobile-nav-toggle"></i>
 				</nav>
