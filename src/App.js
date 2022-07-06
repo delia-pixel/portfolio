c57d91b831ff40f3b7ded4afee815f7b775bf671
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-07-06 17:31:36 +0200

    update App with routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 //import "./App.css";
+import React from "react";
+import  { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./assets/css/style.css";
-import Header from "./components/Header";
+import Home from "./components/Home";
 import About from "./components/About";
 import Resume from "./components/Resume";
 import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <About />
-      <Resume />
-      <Services />
-      <Portfolio />
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
